f25f7ba97299f60553e1314128bbe8062c7ac7e2
dev <dev@seeker>
2013-04-03 15:57:06 -0400

    add view for Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,14 @@ class CommentsController < ApplicationController
     @comment = @shout.comments.create(params[:comment])
     redirect_to shout_path(@shout)
   end
+
+  def index
+    @shout = Shout.find(params[:shout_id])
+    @comments = @shout.comments
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @comments }
+    end
+  end
 end
